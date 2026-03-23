a7fd27cdfe7770ab4e9f9e49d4db8cf49841dcac
sahilshaikh2023 <sahilshaikh6922@gmail.com>
2026-03-23 08:48:13 +0530

    Basic Dom class 48

---
+++ app.js
@@ -0,0 +1,10 @@
+var box =  document.querySelector("#box");
+
+
+box.addEventListener("mouseenter", ()=>{
+    console.log("Enter in the eraaa");
+})
+
+box.addEventListener("mouseleave", ()=>{
+    console.log("leave the eraa");
+})
